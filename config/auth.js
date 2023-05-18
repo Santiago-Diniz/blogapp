66a6356acac143c290a68e08fdc9c10b25f9b8c3
Santiago Diniz <santiagodiniz3@gmail.com>
2023-05-17 23:14:30 -0300

    'ppost'

---
+++ config/auth.js
@@ -0,0 +1,46 @@
+const localStrategy = require('passport-local')
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const passport = require('passport')
+
+require('../models/Usuario')
+const Usuario = mongoose.model('usuarios')
+
+
+module.exports = function(passport){
+
+    passport.use(new localStrategy({usernameField: 'email', passwordField: 'senha'}, (email, senha, done) => {
+
+        Usuario.findOne({email: email}).then((usuario) => {
+            if(!usuario){
+                return done(null, false, {message: 'esta conta não existe'})
+            }
+
+            bcrypt.compare(senha, usuario.senha, (erro, batem) => {
+
+                if(batem){
+                    return done(null, usuario)
+                }else{
+                    return done(null, false, {message: 'Senha incorreta'})
+                }
+            })
+
+        })
+
+    }))
+
+    passport.serializeUser((usuario,done)=>{
+        done(null,usuario.id)
+    })
+    
+    passport.deserializeUser((id,done)=>{
+        Usuario.findById(id).then((usuario)=>{
+            done(null,usuario)
+        }).catch((err)=>{
+             done (null,false,{message:'algo deu errado'})
+        })
+    
+    
+        })
+    
+}
